17f5aa0e78ca4d9ce27a68d6bcda12f6f13e041b
Tobias Raatiniemi <raatiniemi@gmail.com>
2020-04-25 20:16:54 +0200

    improve formatting of Package.swift

--- Package.swift
+++ Package.swift
@@ -1,20 +1,22 @@
 // swift-tools-version:5.0
+
 import PackageDescription
 
 let package = Package(name: "BluetoothLinux",
-            products: [
-                .library(
+        products: [
+            .library(
                     name: "BluetoothLinux",
                     targets: ["BluetoothLinux"]
-                )
-            ],
-            dependencies: [
-                .package(url: "https://github.com/raatiniemi/Bluetooth", .branch("swift-5-default"))
-            ],
-            targets: [
-                .target(name: "BluetoothLinux", dependencies: ["Bluetooth", "CSwiftBluetoothLinux"]),
-                .target(name: "CSwiftBluetoothLinux"),
-                .target(name: "CSwiftBluetoothLinuxTest"),
-                .testTarget(name: "BluetoothLinuxTests", dependencies: ["BluetoothLinux", "CSwiftBluetoothLinuxTest"])
-            ],
-            swiftLanguageVersions: [.v5])
+            )
+        ],
+        dependencies: [
+            .package(url: "https://github.com/raatiniemi/Bluetooth", .branch("swift-5-default"))
+        ],
+        targets: [
+            .target(name: "BluetoothLinux", dependencies: ["Bluetooth", "CSwiftBluetoothLinux"]),
+            .target(name: "CSwiftBluetoothLinux"),
+            .target(name: "CSwiftBluetoothLinuxTest"),
+            .testTarget(name: "BluetoothLinuxTests", dependencies: ["BluetoothLinux", "CSwiftBluetoothLinuxTest"])
+        ],
+        swiftLanguageVersions: [.v5]
+)
